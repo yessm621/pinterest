56e3c5e97f849e4cb0e866d24612111d62de4211
yessm621 <yessm621@gmail.com>
2023-05-20 02:19:36 +0900

    #8 - view 관련 테스트 작성
    보드, Article, 프로필, 구독 관련 테스트를 작성함.

--- src/main/java/com/pinterest/controller/BoardController.java
+++ src/main/java/com/pinterest/controller/BoardController.java
@@ -1,26 +1,32 @@
 package com.pinterest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/boards")
 public class BoardController {
 
     @GetMapping
-    public String boards() {
-        return "/boards/index";
+    public String boards(Model model) {
+        model.addAttribute("boards", List.of());
+        return "boards/index";
     }
 
-    @GetMapping("create")
+    @GetMapping("/create")
     public String create() {
-        return "/boards/create";
+        return "boards/create";
     }
 
     @GetMapping("/{boardId}")
-    public String boardDetail(@PathVariable Long boardId) {
-        return "/boards/detail";
+    public String boardDetail(@PathVariable Long boardId, Model model) {
+        model.addAttribute("board", "board");
+        model.addAttribute("articles", List.of());
+        return "boards/detail";
     }
 }
