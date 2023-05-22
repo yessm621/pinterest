cb5c73f3c2015adbad8a66715743f87ba11a6ff5
yessm621 <yessm621@gmail.com>
2023-05-22 12:48:37 +0900

    #11 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/pinterest/repository/BoardRepository.java
+++ src/main/java/com/pinterest/repository/BoardRepository.java
@@ -1,9 +1,24 @@
 package com.pinterest.repository;
 
 import com.pinterest.domain.Board;
+import com.pinterest.domain.QBoard;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board>,
+        QuerydslBinderCustomizer<QBoard> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QBoard root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.image, root.createdAt);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
 }
--- src/main/java/com/pinterest/repository/CommentRepository.java
+++ src/main/java/com/pinterest/repository/CommentRepository.java
@@ -1,9 +1,24 @@
 package com.pinterest.repository;
 
 import com.pinterest.domain.Comment;
+import com.pinterest.domain.QComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long>, QuerydslPredicateExecutor<Comment>,
+        QuerydslBinderCustomizer<QComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QComment root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.createdAt);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    }
 }
