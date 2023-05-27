8e49ec3e3da0fc458cb377ef8ccb92cdfe6db4b3
yessm621 <yessm621@gmail.com>
2023-05-27 19:59:25 +0900

    #21 - 보드 컨트롤러와 단위 테스트 작성
    보드 컨트롤러에 대한 단위 테스트를 작성하고
    보드 컨트롤러를 테스트를 바탕으로 마저 작성함.

--- src/main/java/com/pinterest/controller/BoardController.java
+++ src/main/java/com/pinterest/controller/BoardController.java
@@ -1,32 +1,45 @@
 package com.pinterest.controller;
 
+import com.pinterest.dto.BoardDto;
+import com.pinterest.dto.BoardWithArticleDto;
+import com.pinterest.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/boards")
+@RequiredArgsConstructor
 public class BoardController {
 
+    private final BoardService boardService;
+
     @GetMapping
-    public String boards(Model model) {
-        model.addAttribute("boards", List.of());
+    public String boards(@RequestParam(required = false) String searchKeyword,
+                         @PageableDefault Pageable pageable,
+                         Model model) {
+        Page<BoardDto> boards = boardService.searchBoards(searchKeyword, pageable);
+        model.addAttribute("boards", boards);
         return "boards/index";
+    }
+
+    @GetMapping("/{boardId}")
+    public String boardDetail(@PathVariable Long boardId, Model model) {
+        BoardWithArticleDto board = boardService.getBoard(boardId);
+        model.addAttribute("board", board);
+        model.addAttribute("articles", board.getArticleDtoList());
+        return "boards/detail";
     }
 
     @GetMapping("/create")
     public String create() {
         return "boards/create";
-    }
-
-    @GetMapping("/{boardId}")
-    public String boardDetail(@PathVariable Long boardId, Model model) {
-        model.addAttribute("board", "board");
-        model.addAttribute("articles", List.of());
-        return "boards/detail";
     }
 }
--- src/test/java/com/pinterest/controller/BoardControllerTest.java
+++ src/test/java/com/pinterest/controller/BoardControllerTest.java
@@ -1,12 +1,25 @@
 package com.pinterest.controller;
 
+import com.pinterest.dto.BoardWithArticleDto;
+import com.pinterest.dto.MemberDto;
+import com.pinterest.service.BoardService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -16,6 +29,9 @@ class BoardControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean
+    BoardService boardService;
+
     public BoardControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -24,6 +40,7 @@ class BoardControllerTest {
     @DisplayName("[View] GET 보드 리스트 페이지 - 정상 호출")
     void givenNothing_whenRequestingBoardsView_thenReturnsBoardsView() throws Exception {
         // Given
+        given(boardService.searchBoards(eq(null), any(Pageable.class))).willReturn(Page.empty());
 
         // When & Then
         mvc.perform(get("/boards"))
@@ -31,20 +48,67 @@ class BoardControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("boards/index"))
                 .andExpect(model().attributeExists("boards"));
+        then(boardService).should().searchBoards(eq(null), any(Pageable.class));
+    }
+
+    @Test
+    @DisplayName("[View] GET 보드 리스트 페이지 - 정상 호출")
+    void givenSearchKeyword_whenRequestingBoardsView_thenReturnsBoardsView() throws Exception {
+        // Given
+        String searchKeyword = "title";
+        given(boardService.searchBoards(eq(searchKeyword), any(Pageable.class))).willReturn(Page.empty());
+
+        // When & Then
+        mvc.perform(
+                        get("/boards")
+                                .queryParam("searchKeyword", searchKeyword)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("boards/index"))
+                .andExpect(model().attributeExists("boards"));
+        then(boardService).should().searchBoards(eq(searchKeyword), any(Pageable.class));
     }
 
     @Test
     @DisplayName("[View] GET 보드 상세 페이지 - 정상 호출")
     void givenNothing_whenRequestBoardView_thenReturnBoardView() throws Exception {
         // Given
+        Long boardId = 1L;
+        given(boardService.getBoard(boardId)).willReturn(createBoardWithArticleDto());
 
         // When & Then
-        mvc.perform(get("/boards/1"))
+        mvc.perform(get("/boards/" + boardId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("boards/detail"))
                 .andExpect(model().attributeExists("board"))
                 .andExpect(model().attributeExists("articles"));
+        then(boardService).should().getBoard(boardId);
+    }
 
+    private BoardWithArticleDto createBoardWithArticleDto() {
+        return BoardWithArticleDto.of(
+                1L,
+                createMemberDto(),
+                new ArrayList<>(),
+                "보드 타이틀",
+                "http://dummyimage.com/246x205.png/dddddd/000000",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+    }
+
+    private MemberDto createMemberDto() {
+        return MemberDto.of(
+                1L,
+                "yessm621@gmail.com",
+                "test123",
+                "yessm",
+                "승미입니다.",
+                "image",
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
     }
 }
