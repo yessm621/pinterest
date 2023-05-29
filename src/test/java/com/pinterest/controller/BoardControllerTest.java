1cd864e8964793c1bb6fdc478dbf7edbff89193f
yessm621 <yessm621@gmail.com>
2023-05-30 02:49:56 +0900

    #25 - 보드에 페이징, 정렬 기능을 추가
    보드 컨트롤러에 페이징 기능과 정렬 기능을 추가하고 테스트 코드를 작성함.

--- src/test/java/com/pinterest/controller/BoardControllerTest.java
+++ src/test/java/com/pinterest/controller/BoardControllerTest.java
@@ -3,21 +3,24 @@ package com.pinterest.controller;
 import com.pinterest.dto.BoardWithArticleDto;
 import com.pinterest.dto.MemberDto;
 import com.pinterest.service.BoardService;
+import com.pinterest.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -32,6 +35,9 @@ class BoardControllerTest {
     @MockBean
     BoardService boardService;
 
+    @MockBean
+    PaginationService paginationService;
+
     public BoardControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -41,14 +47,18 @@ class BoardControllerTest {
     void givenNothing_whenRequestingBoardsView_thenReturnsBoardsView() throws Exception {
         // Given
         given(boardService.searchBoards(eq(null), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
 
         // When & Then
         mvc.perform(get("/boards"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("boards/index"))
-                .andExpect(model().attributeExists("boards"));
+                .andExpect(model().attributeExists("boards"))
+                .andExpect(model().attributeExists("pagination"));
+
         then(boardService).should().searchBoards(eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
     @Test
@@ -57,6 +67,7 @@ class BoardControllerTest {
         // Given
         String searchKeyword = "title";
         given(boardService.searchBoards(eq(searchKeyword), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
 
         // When & Then
         mvc.perform(
@@ -66,8 +77,40 @@ class BoardControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("boards/index"))
-                .andExpect(model().attributeExists("boards"));
+                .andExpect(model().attributeExists("boards"))
+                .andExpect(model().attributeExists("pagination"));
+
         then(boardService).should().searchBoards(eq(searchKeyword), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @Test
+    @DisplayName("[View] GET 게시글 리스트 (게시판) 페이지 - 페이징, 정렬 기능")
+    void givenPagingAndSortingParams_whenSearchingBoardsView_thenReturnsBoardsView() throws Exception {
+        // Given
+        String sortName = "title";
+        String direction = "desc";
+        int pageNumber = 0;
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc(sortName)));
+        List<Integer> barNumbers = List.of(1, 2, 3, 4, 5);
+        given(boardService.searchBoards(null, pageable)).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages())).willReturn(barNumbers);
+
+        // When & Then
+        mvc.perform(
+                        get("/boards")
+                                .queryParam("page", String.valueOf(pageNumber))
+                                .queryParam("size", String.valueOf(pageSize))
+                                .queryParam("sort", sortName + "," + direction)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("boards/index"))
+                .andExpect(model().attributeExists("boards"))
+                .andExpect(model().attribute("pagination", barNumbers));
+        then(boardService).should().searchBoards(null, pageable);
+        then(paginationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
     }
 
     @Test
