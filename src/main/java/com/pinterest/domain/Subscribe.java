992e5955f71720a9f80d2d61132943af37119937
yessm621 <yessm621@gmail.com>
2023-07-10 00:31:54 +0900

    #41 - Subscribe 엔티티와 리포지토리 추가

---
+++ src/main/java/com/pinterest/domain/Subscribe.java
@@ -0,0 +1,51 @@
+package com.pinterest.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@ToString(exclude = {"member", "article"})
+public class Subscribe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscribe_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+
+    protected Subscribe() {
+    }
+
+    private Subscribe(Member member, Article article) {
+        this.member = member;
+        this.article = article;
+    }
+
+    public static Subscribe of(Member member, Article article) {
+        return new Subscribe(member, article);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Subscribe)) return false;
+        Subscribe subscribe = (Subscribe) o;
+        return id != null && Objects.equals(getId(), subscribe.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
